60c0906ca99209c4f441f89e4417f904614f874b
Jonathan Kim <jonnykimbk@hotmail.com>
2023-05-02 00:41:01 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Links from "./components/links/links";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Links />
         {children}
+        <Analytics />
       </body>
     </html>
   );
