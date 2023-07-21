e1d66fce1e159deef25c102dc626ed8c9f720056
Jonathan Kim <jonnykimbk@hotmail.com>
2023-07-20 21:52:02 -0300

    bug

--- src/app/api/today/route.ts
+++ src/app/api/today/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const todayValue = await fetch("https://api.bluelytics.com.ar/v2/latest", {
-    next: { revalidate: 60 },
+    next: { revalidate: 0 },
   }).then((value) => {
     return value.json();
   });
