bc28a22b3b4965a9b0051e663a1aeed59a9eff1c
Jonathan Kim <jonnykimbk@hotmail.com>
2023-07-24 13:36:19 -0300

    today value change

--- src/app/api/today/route.ts
+++ src/app/api/today/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const todayValue = await fetch("https://api.bluelytics.com.ar/v2/latest", {
-    next: { revalidate: 0 },
-  }).then((value) => {
-    console.log(Date());
-
-    return value.json();
-  });
+  const todayValueResponse = await fetch(
+    "https://api.bluelytics.com.ar/v2/latest",
+    {
+      next: { revalidate: 0 },
+    }
+  );
+  const todayValue = await todayValueResponse.json();
 
   return NextResponse.json(todayValue);
 }
