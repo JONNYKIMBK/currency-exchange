f0650251fc898ab4d3129767c2f990110aef9257
Jonathan Kim <jonnykimbk@hotmail.com>
2023-07-28 15:21:10 -0300

    no cache

--- src/app/api/today/route.ts
+++ src/app/api/today/route.ts
@@ -5,6 +5,9 @@ export async function GET(request: Request) {
     "https://api.bluelytics.com.ar/v2/latest",
     {
       next: { revalidate: 0 },
+      headers: {
+        "Cache-Control": "no-cache",
+      },
     }
   );
   const todayValue = await todayValueResponse.json();
